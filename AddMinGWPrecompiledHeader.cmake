8f6dd5ebe2f5818916401260c96035e779e1fe87
Fadhil Mandaga <firodj@gmail.com>
2016-08-03 09:47:57 +0700

    Mingw pch using build dir instead original incl dir

--- AddMinGWPrecompiledHeader.cmake
+++ AddMinGWPrecompiledHeader.cmake
@@ -3,9 +3,12 @@ macro(ADD_MINGW_PRECOMPILED_HEADER PrecompiledHeader ProjectName)
     get_directory_property(DirIncs INCLUDE_DIRECTORIES)
 
     find_path(${ProjectName}_PrecompiledPath NAMES ${PrecompiledHeader} PATHS ${DirIncs})
-    
     set(PREC_INPUT "${${ProjectName}_PrecompiledPath}/${PrecompiledHeader}")
-    set(PREC_OUTPUT "${PREC_INPUT}.gch")    
+
+    get_filename_component(${ProjectName}_PrecompiledBasename ${PREC_INPUT} NAME)
+    
+    set(PREC_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${${ProjectName}_PrecompiledBasename}.gch")
+
     set(PREC_FLAGS
       -g
       -c "${PREC_INPUT}" 
@@ -15,7 +18,7 @@ macro(ADD_MINGW_PRECOMPILED_HEADER PrecompiledHeader ProjectName)
     )
 
     foreach(include_dir ${DirIncs})
-      list(APPEND PREC_FLAGS -I "${include_dir}")
+      list(APPEND PREC_FLAGS -I"${include_dir}")
     endforeach()
 
     get_directory_property(DirDefs COMPILE_DEFINITIONS)
@@ -31,6 +34,10 @@ macro(ADD_MINGW_PRECOMPILED_HEADER PrecompiledHeader ProjectName)
     add_custom_target(${ProjectName}_generate_precompiled DEPENDS "${PREC_OUTPUT}")
   
     add_dependencies(${ProjectName} ${ProjectName}_generate_precompiled)
+
+    get_target_property(C_FLAGS ${ProjectName} COMPILE_FLAGS)
+    set(C_FLAGS "${C_FLAGS} -Winvalid-pch -include ${PrecompiledHeader}")
+    set_target_properties(${ProjectName} PROPERTIES COMPILE_FLAGS ${C_FLAGS})
 
   endif(MINGW)
 endmacro(ADD_MINGW_PRECOMPILED_HEADER)
